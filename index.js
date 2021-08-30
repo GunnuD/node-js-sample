23ad17cfa6dc195b35417b32ce4995e88c0bae37
GunnuD <38685931+GunnuD@users.noreply.github.com>
2021-08-30 15:43:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello DSPA, Dear Seema , This is Testing DevOps Node! JS Project , Hello ,Aarti Rajput How are you ? welcome to Pooja Rajpoot  devOps Project---Thanks DSPA cloud technology')
+  response.send('Hello DSPA, devOps Project---Thanks DSPA cloud technology')
 }) 
 
 app.listen(app.get('port'), function() {
