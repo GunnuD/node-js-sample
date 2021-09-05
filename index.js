86f9f3eab4918d83368ea18e75668b527475e011
GunnuD <38685931+GunnuD@users.noreply.github.com>
2021-09-05 20:59:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello DSPA, devOps Project---Thanks DSPA cloud technology')
+  response.send('Hello DSPA, devOps Project---Thanks DSPA cloud technology Welcome to Faridabad')
 }) 
 
 app.listen(app.get('port'), function() {
